7eaefe15d6691e7b1560efbf2ba3ffa6bf09ad10
Andy Macdonald <hello@andymacdonald.dev>
2022-11-10 12:02:09 +0000

    Initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,25 @@
-import { render, screen } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+const tableHeadings = ["Id", "Name", "Username", "Email", "Address street", "Phone", "Website", "Company name"];
+
+describe('userMismanagement', () => {
+
+  test('renders user table', async () => {
+    const { getByText } = render(<App />);
+    await waitFor(() => {
+      expect(getByText(/Users/i)).toBeVisible()
+    })
+  });
+
+  test.each(tableHeadings)(
+    "renders %s heading as expected",
+    async (heading) => {
+      const { getByText } = render(<App />);
+      await waitFor(() => {
+        expect(getByText(heading)).toBeVisible()
+      })
+    }
+  );
+
+})
